bcaebb183743f3a7956800e23ba215ce9210625f
parkerdunbar <PDunbar@student.neumont.edu>
2018-10-15 12:09:53 -0600

    Basic example

--- Multi-Theaded-Server/src/main/java/server/Client.java
+++ Multi-Theaded-Server/src/main/java/server/Client.java
@@ -1,5 +1,28 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Client {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		while (true) {
+			try (Socket sock = new Socket("localhost", 2500)) {
+				System.out.println("Connection established");
+				try (PrintStream out = new PrintStream(sock.getOutputStream())) {
+					System.out.println("Sending text: \"Hello\"");
+					out.println("Hello");
+					BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+					String line = in.readLine();
+					System.out.println("Server said: \"" + line + "\"");
+				}
+			}
+		}
+	}
 
 }
--- Multi-Theaded-Server/src/main/java/server/Server.java
+++ Multi-Theaded-Server/src/main/java/server/Server.java
@@ -1,5 +1,35 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
+	public static void main(String[] args) throws IOException, InterruptedException {
+		ServerSocket servSocket = new ServerSocket(2500);
+		System.out.println("Starting Up...");
+		
+		try (Socket s = servSocket.accept()) {
+			System.out.println("Connection recieved");
+			
+			InputStream sockIn = s.getInputStream();
+			BufferedReader sockReader = new BufferedReader(new InputStreamReader(sockIn));
+			String line = sockReader.readLine();
+			System.out.println("Read the line \"" + line + "\"");
+			
+			PrintStream sockOut = new PrintStream(s.getOutputStream());
+			sockOut.println(line);
+			System.out.println("Echoed the line back to the client");
+			Thread.sleep(1000);
+			sockOut.flush();
+			
+			System.out.println("Closing connection");
+			servSocket.close();
+		}
+	}
 }
